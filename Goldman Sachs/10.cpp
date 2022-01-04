b8b4f88ec4bb66365a2edefc1e3e603e5dd6125a
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2022-01-04 16:31:49 +0530

    Create 10.cpp

---
+++ Goldman Sachs/10.cpp
@@ -0,0 +1,3 @@
+I will maintain a priority queue of size 10 and will insert the first 10 elements in the queue. After that, I will iterate through the numbers, if I find any number larger than the head of priority queue, I will swap them.
+  This method is better than sorting all the elements either in descending or ascending numbers to look for the largest 10 numbers.
+    Thank You.
