28a3a078391821609a19ddb79c42ab9c3e9749bd
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2022-01-09 17:57:27 +0530

    Create 10.cpp

---
+++ Amazon/10.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) {
+	    // code here
+	    sort(nuts, nuts+n);
+	    sort(bolts, bolts+n);
+	}
+// #= 35
+// $ = 36
+// % = 37
+// ^= 94
+// @ = 64
+};
