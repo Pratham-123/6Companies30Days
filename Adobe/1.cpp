5dec45337e2f10a3f03f0e5085c9a1c6de7a8232
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2022-01-18 12:43:27 +0530

    Create 1.cpp

---
+++ Adobe/1.cpp
@@ -0,0 +1,27 @@
+vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        vector<int> res(2,-1);
+        int l=0,r=0;
+        long long sum=0;
+        
+        while(r<n){
+            
+            if(sum+arr[r]==s){
+                res[0]=l+1;
+                res[1]=r+1;
+                break;
+            }
+            else if(sum+arr[r]<s){
+                sum+=arr[r];
+                r++;
+            }
+            else {
+                sum-=arr[l];
+                l++;
+            }
+        }
+        
+        if(res[0]==-1 && res[1]==-1)
+        res.pop_back();
+        return res;
+    }
