9d8004194c8c7b6bc2ab8a9fab23bdbb68064d60
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2022-01-19 18:22:22 +0530

    Create 11.cpp

---
+++ Adobe/11.cpp
@@ -0,0 +1,13 @@
+ public:
+    string amendSentence (string s)
+    {
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(i!=0 && isupper(s[i]))
+            {
+                res+=" ";
+            }
+            res+=tolower(s[i]);
+        }
+        return res;
+    }
